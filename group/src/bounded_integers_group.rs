c6bcacffbae6823bb5db51f4af51b39cbabe8a7a
Yehonatan Cohen Scaly <scalys7@gmail.com>
2025-11-30 01:26:13 +0200

    Relax the randomizer upper bound for a temp solution (security holds)

--- group/src/bounded_integers_group.rs
+++ group/src/bounded_integers_group.rs
@@ -72,7 +72,7 @@ where
             .checked_add(MAURER_PROOFS_DIFF_UPPER_BOUND_BITS)
             .ok_or(Error::InvalidPublicParameters)?;
 
-        Self::new(sample_bits, upper_bound_bits)
+        Self::new(sample_bits, upper_bound_bits - 10)
     }
 }
 
